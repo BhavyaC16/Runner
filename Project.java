286929cbff5259da8330535bfea5353acf4316f7
Bhavya <bhavya18333@iiitd.ac.in>
2019-08-01 22:00:15 +0530

    Added linked list to store gui inputs

--- Project.java
+++ Project.java
@@ -9,11 +9,18 @@ public class Project {
 
     public static JPanel p_name;
     public static JPanel p_time;
+    public static JPanel p_message;
     public static JLabel l_name;
     public static JLabel l_time;
+    public static JLabel l_message;
     public static JTextField tf_name;
     public static JTextField tf_time;
+    public static JTextField tf_message;
+    public static Linked_List linkedlist;
     public static void main(String[] args) {
+
+        
+        linkedlist = new Linked_List();
 
         JFrame frame = new JFrame("Runners");
         JPanel p_main = new JPanel();
@@ -78,11 +85,165 @@ public class Project {
         p_buttons.add(b_cancel);
         p_main.add(p_buttons);
 
+
+        b_next.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String name = tf_name.getText();
+                int time = Integer.parseInt(tf_time.getText());
+                boolean hm = rb_HalfMarathon.isSelected();
+                boolean gdr = rb_GreatDelhiRun.isSelected();
+                boolean otr = rb_OpenRun.isSelected();
+                linkedlist.insert(name,time,hm,gdr,otr);
+                System.out.println("added");
+            }
+        });
+
+        //Message Panel/////////////////////////////////////////
+        JPanel p_message = new JPanel();
+        p_message.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        JLabel l_message = new JLabel("Winners: ");
+        JTextField tf_message = new JTextField();
+        tf_message.setPreferredSize(new Dimension(150,50));
+        p_message.add(l_message);
+        p_message.add(tf_message);
+        p_main.add(p_message);
+
+
         //Frame Display///////////////////////////////////////
         frame.add(p_main);
         frame.setSize(600,600);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+    }
+}
+class Runner
+    { 
+        private String name;
+        private int time;
+        private boolean hm,gdr,otr;
+        Runner next;
+
+        public Runner()
+        {
+            this.name = "";
+            this.time = 0;
+            this.hm = false;
+            this.gdr = false;
+            this.otr = false;
+            next = null;
+        }
+
+        public Runner(String name, int time, boolean hm, boolean gdr, boolean otr)
+        {
+            this.name = name;
+            this.time = time;
+            this.hm = hm;
+            this.gdr = gdr;
+            this.otr = otr;
+            next = null;
+        }
+    }
+class Linked_List 
+{ 
+    static int winHM;
+    static int secHM;
+    static int winGDR;
+    static int secGDR;
+    static int winOTR;
+    static int secOTR;
+ 
+    static Runner head = null;
+    static Runner win1 = null;
+    static Runner sec1 = null;
+    static Runner win2 = null;
+    static Runner sec2 = null;
+    static Runner win3 = null;
+    static Runner sec3 = null;
+
+
+    static void insert(String name, int time, boolean hm, boolean gdr, boolean otr) 
+    {
+        Runner  ptr_to_insert = new Runner(name,time,hm,gdr,otr);
+        ptr_to_insert.next = head;
+        head = ptr_to_insert;
+        if(hm==true)
+        {
+            if(win1==null)
+            {
+                win1 = ptr_to_insert;
+                winHM = time;
+            }
+            else if(sec1==null && time>winHM)
+            {
+                sec1 = ptr_to_insert;
+                secHM = time;
+            }
+            else if(time<winHM)
+            {
+                sec1 = win1;
+                secHM = winHM;
+                win1 = ptr_to_insert;
+                winHM = time;
+            }
+            else if(time>winHM && time<secHM)
+            {
+                sec1 = ptr_to_insert;
+                secHM = time;
+            }
+        }
+        else if(gdr==true)
+        {
+            if(win2==null)
+            {
+                win2 = ptr_to_insert;
+                winGDR = time;
+            }
+            else if(sec2==null && time>winGDR)
+            {
+                sec2 = ptr_to_insert;
+                secGDR = time;
+            }
+            else if(time<winGDR)
+            {
+                sec2 = win2;
+                secGDR = winGDR;
+                win2 = ptr_to_insert;
+                winGDR = time;
+            }
+            else if(time>winGDR && time<secGDR)
+            {
+                sec2 = ptr_to_insert;
+                secGDR = time;
+            }
+        }
+        else if(otr==true)
+        {
+            if(win3==null)
+            {
+                win3 = ptr_to_insert;
+                winOTR = time;
+            }
+            else if(sec3==null && time>winOTR)
+            {
+                sec3 = ptr_to_insert;
+                secOTR = time;
+            }
+            else if(time<winOTR)
+            {
+                sec3 = win3;
+                secOTR = winOTR;
+                win3 = ptr_to_insert;
+                winOTR = time;
+            }
+            else if(time>winOTR && time<secOTR)
+            {
+                sec3 = ptr_to_insert;
+                secOTR = time;
+            }
+        }
     }
 }
